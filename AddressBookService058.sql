b329d638a7739fa4f1803340dd51b499dc34f896
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-27 12:19:35 +0530

    [ShubhamPV] Inserting Details in AddressBook Table

--- AddressBookService058.sql
+++ AddressBookService058.sql
@@ -14,4 +14,12 @@ ZipCode int,
 PhoneNum bigint,
 EmailId varchar(100)
 );
-select * from AddressBook
+select * from AddressBook;
+
+--UC3 Inserting Details in Table --
+insert into AddressBook values('Bhushan', 'Kapale','Ulhasnagar','Thane','Maharashtra',421004,8888126666,'BhushanKapale@gmail.com'),
+('Niraj', 'Kapale','kalyan','Thane','Maharashtra',421005,8888136666,'NirajKapale@gmail.com'),
+('Siddhant','Wani','badlapur','Thane','Maharashtra',421006,8888146666,'SiddhantWani@gmail.com'),
+('Shubham', 'Vadnere','Mulund','Thane','Maharashtra',421007,8888156666,'ShubhamVadnere@gmail.com'),
+('Pavan', 'Patil','Ambernath','Thane','Maharashtra',421008,8888166666,'PavanPatil@gmail.com')
+select * from AddressBook;s
