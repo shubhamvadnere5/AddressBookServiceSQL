ececd4e30ac45903f5d180abdea524739c0f4847
ShubhamPV <shubhamvadnere5@gmail.com>
2021-10-27 11:15:59 +0530

    [ShubhamPV] Creating Table AddressBook

--- AddressBookService058.sql
+++ AddressBookService058.sql
@@ -1,2 +1,17 @@
----UC1---
+---UC1 Create Database--
 create database AddressBookService058;
+
+--UC2 Create Table--
+use AddressBookService058;
+create table AddressBook
+(
+FirstName varchar(100) not null,
+LastName varchar(50),
+Address Varchar(200),
+city varchar(150),
+StateName varchar(100),
+ZipCode int,
+PhoneNum bigint,
+EmailId varchar(100)
+);
+select * from AddressBook
